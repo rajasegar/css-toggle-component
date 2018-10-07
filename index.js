51c88416f99ae0a07b0106fa2007abecf56f8cad
Rajasegar <rajasegarc@freshdesk.com>
2018-10-07 10:34:24 +0530

    New styles added and readme update

--- index.js
+++ index.js
@@ -270,6 +270,175 @@ transform: rotateY(-180deg);
 transition: 0.3s ease all;
 }
 
+/* Rotate style */
+.rotate
+{
+overflow: visible;
+}
+
+.rotate .knobs:before
+{
+content: attr(data-yes);
+position: absolute;
+top: 4px;
+left: 4px;
+width: 20px;
+height: 10px;
+color: #fff;
+font-size: 10px;
+font-weight: bold;
+text-align: center;
+line-height: 1;
+padding: 9px 4px;
+background-color: #03A9F4;
+border-radius: 50%;
+}
+
+.rotate .layer, .rotate .knobs, .rotate .knobs:before
+{
+transform: rotateZ(0);
+transition: 0.4s cubic-bezier(0.18, 0.89, 0.35, 1.15) all;
+}
+
+.rotate .checkbox:checked + .knobs
+{
+transform: rotateZ(-180deg);
+}
+
+.rotate .checkbox:checked + .knobs:before
+{
+content: attr(data-no);
+         background-color: #f44336;
+transform: rotateZ(180deg);
+}
+
+.rotate .checkbox:checked ~ .layer
+{
+  background-color: #fcebeb;
+transform: rotateZ(180deg);
+}
+
+/* Fade out */
+.fadeout .knobs:before, .fadeout .knobs:after, .fadeout .knobs span
+{
+position: absolute;
+top: 4px;
+width: 20px;
+height: 10px;
+        font-size: 10px;
+        font-weight: bold;
+        text-align: center;
+        line-height: 1;
+padding: 9px 4px;
+         border-radius: 50%;
+transition: 0.3s ease all;
+}
+
+.fadeout .knobs:before
+{
+content: attr(data-yes);
+color: #fff;
+left: 4px;
+}
+
+.fadeout .knobs:after
+{
+content: attr(data-no);
+left: 42px;
+color: #fff;
+       background-color: #f44336;
+opacity: 0;
+}
+
+.fadeout .knobs:before, .fadeout .knobs:after
+{
+  z-index: 2;
+}
+
+.fadeout .knobs span
+{
+left: 4px;
+      background-color: #03A9F4;
+      z-index: 1;
+}
+
+.fadeout .checkbox:checked + .knobs:before
+{
+opacity: 0;
+}
+
+.fadeout .checkbox:checked + .knobs:after
+{
+opacity: 1;
+}
+
+.fadeout .checkbox:checked + .knobs span
+{
+  background-color: #fcebeb;
+transform: scale(4);
+}
+
+/* Slide all */
+.slideall .knobs:before, .slideall .knobs:after, .slideall .knobs span
+{
+    position: absolute;
+    top: 4px;
+    width: 20px;
+    height: 10px;
+    font-size: 10px;
+    font-weight: bold;
+    text-align: center;
+    line-height: 1;
+    padding: 9px 4px;
+    border-radius: 50%;
+    transition: 0.4s cubic-bezier(0.18, 0.89, 0.35, 1.15) all;
+}
+
+.slideall .knobs:before
+{
+    content: attr(data-yes);
+    left: 4px;
+}
+
+.slideall .knobs:after
+{
+    content: attr(data-no);
+    right: -24px;
+}
+
+.slideall .knobs:before, .slideall .knobs:after
+{
+    color: #fff;
+    z-index: 2;
+}
+
+.slideall .knobs span
+{
+    left: 4px;
+    background-color: #03a9f4;
+    z-index: 1;
+}
+
+.slideall .checkbox:checked + .knobs:before
+{
+    left: -24px;
+}
+
+.slideall .checkbox:checked + .knobs:after
+{
+    right: 4px;
+}
+
+.slideall .checkbox:checked + .knobs span
+{
+    left: 42px;
+    background-color: #F44336;
+}
+
+.slideall .checkbox:checked ~ .layer
+{
+    background-color: #fcebeb;
+}
 </style>
 <div class="button r" role="switch" aria-label="CSS Toggle Button" aria-checked="true">
   <input type="checkbox" class="checkbox" aria-label="CSS Toggle Button">
@@ -293,15 +462,12 @@ transition: 0.3s ease all;
       const knobs = this.shadowRoot.querySelector(".knobs");
       knobs.setAttribute("data-yes", onLabel);
       knobs.setAttribute("data-no", offLabel);
-      if (theme === "flip") {
-        //const span = document.createElement("span");
-        //span.setAttribute("data-yes", onLabel);
-        //span.setAttribute("data-no", offLabel);
-        //knobs.appendChild(span);
-
-        const span = (yes, no) =>
-          `<span data-yes="${yes}" data-no="${no}"></span>`;
-        knobs.innerHTML = span(onLabel, offLabel);
+      const addSpan = ["flip", "fadeout", "slideall"];
+      if (addSpan.includes(theme)) {
+        const span = document.createElement("span");
+        span.setAttribute("data-yes", onLabel);
+        span.setAttribute("data-no", offLabel);
+        knobs.appendChild(span);
       }
     }
   }
